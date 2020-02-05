938cd98f967739d53b3496161aa6fc40609eca51
zanllp <zanllp@163.com>
2020-02-05 23:08:56 +0800

    修改api

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { reset } from './reset';
 
 export const DEV = false;
 export const BACKEND_BASE = DEV ? 'http://127.0.0.1:7001' : 'https://api.ioflow.link';
-export const CSRF = DEV ? 'rd6MjdiILDJ7hSW4feK8vQiC' : '26Rpk7Hm-8VQdRilUkgIRqgZ';
+export const CSRF = DEV ? 'rd6MjdiILDJ7hSW4feK8vQiC' : 'l38JTxitsxcltQtYnt08xjBa';
 export const CSRF_HEADER = { 'x-csrf-token': CSRF };
 export const JSON_HEADER = { 'content-type': 'application/json' };
 export const PLUGIN_DIR = './plugin';
 export const COOKIE = DEV
     ? { Cookie: 'token=G6SvgfsqFCZvB7JJUtTlCdevSh4HDu4XQXO3gXB3TxBlBgYIgD5eGCWt8c3EtjHr; token.sig=TvCg_5L3mkhadCbPUbBTjLCzLHEbSLk0DznlPyQvXAQ; csrfToken=rd6MjdiILDJ7hSW4feK8vQiC' }
-    : { Cookie: 'csrfToken=26Rpk7Hm-8VQdRilUkgIRqgZ; token=6vNsC4tKcqfveTCMBEijUn3fmgChpQDcq2WXdVsFVTmgqftycv5FsF84rJqu5GDt; token.sig=vhYvjw_O91f9tajarGG4UBXezLz600EwS7BO01aQ4Fk' };
+    : { Cookie: 'csrfToken=l38JTxitsxcltQtYnt08xjBa; token=i0eSJMgc14SIGdlaE9DQPOZCgWhgcjICJ0CY3POf8HHPXDWwJqqp1m9FIxClH395; token.sig=2Sh30cTg-6aWM4JWOi6AYAtaqHW1NQpCaH8GE8zha3c' };
 
 const run = async () => {
     const local = await localInfo();
